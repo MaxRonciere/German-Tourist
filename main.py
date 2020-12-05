c7805baf772a6b51abc38de314fded31917a5966
Max Roncière <max.ronciere@receperunt.com>
2020-12-05 20:04:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from  ThickBot import Bot
 from discord.ext import commands
 
-reponse_rage = [
-    "ptin je t'ai rien dit depuis longtemps ques-tu me veut la ? la merde",
-    "De toute façon tu as toujours raison et jamais tord",
-    "Vas y je te laisse penser que la terre est plate",
-    "Mais qu'est ce que tu me racompte"
-    ]
 
 
 if __name__ == '__main__':
-    jeton ="YOUR TOKEN"
-    bot = commands.Bot(command_prefix='cooking ')
-    bot.add_cog(Bot(bot,reponse_rage))
+    path = "database/sentences.txt"
+    jeton =""  # token
+    prefix = 'dridri'
+
+
+    bot = commands.Bot(command_prefix=f'{prefix} ')
+    bot.add_cog(Bot(bot,path))
     bot.run(jeton)
     print("See you next time")
