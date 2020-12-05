27c33e1ef760f9445625d734415ae4582dd2b1ad
Max Roncière <max.ronciere@receperunt.com>
2020-12-05 17:52:37 +0100

    Update ThickBrain.py

--- ThickBrain.py
+++ ThickBrain.py
@@ -1,9 +1,6 @@
 import asyncio
-from aiofile import AIOFile, Reader, Writer
 
 import markovify
-from random import choice
-import concurrent.futures
 import sys
 
 class Brain(object):
@@ -12,25 +9,15 @@ class Brain(object):
     def __init__(self, arg):
         self.path = arg
 
-    def think(self,iteration=1, thread_number=1):
-        result = []
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            for i in range(thread_number):
-                thread = executor.submit(self.worker, iteration)
-                result.append(thread.result())
-        new_list = []
-        for i in result :
-            if i not in new_list:
-                new_list.append(i)
-        return new_list
 
-    def worker(self,i):
+    def think(self,i):
         list = []
         with open(self.path) as f:
             text = f.read()
-        text_model = markovify.NewlineText(text)
+        text_model = markovify.Text(text, retain_original=False)
+        text_model.compile(inplace = True)
         for i in range(i):
-            a = text_model.make_short_sentence(100)
+            a = text_model.make_short_sentence(280)
             list.append(a)
         return list
 
@@ -42,19 +29,22 @@ class Save(object):
         self.path = arg
 
     def save(self,data):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.run(data))
+        with open(self.path,'r') as f:
+            for piece in self.read_in_chunks(f):
+                if data in piece:
+                    return
+        with open(self.path,'a') as f:
+            f.write(data)
+            f.write('\n')
+            f.close()
+    
+    def read_in_chunks(self,file_object, chunk_size=1024):
+        while True:
+            data = file_object.read(chunk_size)
+            if not data:
+                break
+            yield data
 
 
-    async def run(self,data):
-        found  = False
-        async with AIOFile(self.path, mode='r') as afp:
-            reader = Reader(afp, chunk_size=8)
-            async for chunk in reader:
-                if data in str(chunk):
-                    found = True
-                    sys.stdout.write('data already exist\n')
-        if found is False:
-            async with AIOFile(self.path, mode='a') as afp:
-                await afp.write(str(data))
-                await afp.write('\n')
+
+
